74f70bd33b7230e15e29496163d61e586997adc6
radadiyaneel <68851688+radadiyaneel@users.noreply.github.com>
2020-10-12 11:52:15 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,16 @@
+package com.employee;
+
+public class Employee {
+
+    String Name,City;
+    int age;
+    Void display(){
+        System.out.println("The name is " + this.Name);
+        System.out.println("The age is " + this.age);
+        System.out.println("The city is" + this.City);
+
+        return null;
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+       Employee e = new Employee();
+       e.Name="Neel";
+       e.age=20;
+       e.City="Ahmedabad";
+       Employee e1 = new Employee();
+       e1.Name="Urvish";
+       e1.age=20;
+       e1.City="Ahmedabad";
+       e.display();
+       e1.display();
+
+
+
+
+
+    }
+}
